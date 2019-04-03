e730891349f3b91945047f23d1993a838cd055bc
Evgeny Vetlugin <vetlugin@spherasveta.ru>
2019-04-03 09:01:09 +0300

    Добавлены новые функции

--- fetch_vacancies.py
+++ fetch_vacancies.py
@@ -1,16 +1,38 @@
 import requests
 
-def get_vacancies():
+def get_vacancies(lang):
     vac_path = 'https://api.hh.ru/vacancies/'
 
     payload = {
         'text':'программист',
-        'text':'python',
+        'text':lang,
+        'area':'3',
+        'period': '30',
     }
     response = requests.get(vac_path, params=payload)
     return response
 
+def get_lang_rating(count):
+
+    langs = ['JavaScript', 'Java', 'Python', 'Ruby','PHP','C++','C','Go','Objective-C','Scala','Swift','C#']
+
+    lang_rating = {}
+    for lang in langs:
+        vacancies = get_vacancies(lang).json()['found']
+        if vacancies >= count:
+            lang_rating[lang] = vacancies
+
+    return lang_rating
+
+def get_salary_by_lang(lang):
+
+    vacancies = get_vacancies(lang).json()['items']
+
+    salary = []
+    for i in range(len(vacancies)):
+        salary.append(vacancies[i]['salary'])
+    return salary
+
 if __name__ == '__main__':
-    response = get_vacancies().json()['items']
-    for vacancy in range(len(response)):
-        print(response[vacancy]['name'])
+    #print(get_lang_rating(150))
+    print(get_salary_by_lang('python'))
